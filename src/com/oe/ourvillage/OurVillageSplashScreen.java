f43d95f47de89ac1e338b267db785d57cb3d73bb
Serete Itebete <sitebete@gmail.com>
2010-11-29 21:39:22 -0800

    added new splash screen image and dialog box

--- src/com/oe/ourvillage/OurVillageSplashScreen.java
+++ src/com/oe/ourvillage/OurVillageSplashScreen.java
@@ -2,12 +2,17 @@ package com.oe.ourvillage;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 
 
 public class OurVillageSplashScreen extends Activity {
 
+    AlertDialog alert;
+
+    AlertDialog.Builder builder;
     private final int SPLASH_DISPLAY_TIME = 3500;
 
     @Override
@@ -16,10 +21,23 @@ public class OurVillageSplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(OurVillageSplashScreen.this);
+        builder.setMessage("Press OK To Continue").setCancelable(false)
+               .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+                   @Override
+                   public void onClick(DialogInterface dialog, int id) {
+                       // TODO Auto-generated method stub
+                       OurVillageSplashScreen.this.finish();
+                   }
+               });
+        alert = builder.create();
+
         // handler of display
         new Handler().postDelayed(new Runnable() {
             public void run() {
-                OurVillageSplashScreen.this.finish();
+
+                alert.show();
             }
         }, SPLASH_DISPLAY_TIME);
 
